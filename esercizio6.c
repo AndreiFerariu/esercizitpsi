ef97050b1ebc71c46f0f1d9c4471e324592b88e9
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2022-02-03 19:20:10 +0100

    esercizio6

---
+++ esercizio6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+int a,b,c,media, sentinella = 0, somma;
+
+
+do
+ {
+ printf("inserisci a:\n" );
+ scanf("%d", &a);
+ printf("inserisci b (un valore maggiore di a):\n" );
+ scanf("%d", &b); 
+ } while (a > b || a == b);
+do
+{
+ printf("inserisci c:\n" );
+ scanf("%d", &c);
+sentinella++;
+somma = somma + c;
+
+} while (c < b && c > a);
+media = (double) somma / (double) sentinella; 
+printf("La media è:\n", media );
+
+}
