e5589e683d60ec4a1068b4dd9c402de4a3f83105
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2022-02-03 18:43:22 +0100

    migliorato

--- esercizio3.c
+++ esercizio3.c
@@ -2,18 +2,20 @@
 int main(int argc, char *argv[])
 {
     int a, b, rapporto, radice;
-    printf("inserisci a:");
+    do
+    {printf("inserisci a:");
     scanf("%d", &a);
     printf("inserisci b:");
     scanf("%d", &b);
     if (a > b)
-        rapporto = a / b;
+        rapporto = (double)a / (double)b;
     else
-        rapporto = b / a
+        rapporto = (double)b / (double)a;
 
-                   if (rapporto < 0)
-                       printf("valore non identificato perchè i numeri sono negativi");
-    else radice = sqrt(rapporto);
-
+   if (rapporto > 0)
+  radice = sqrt(rapporto);
+    prinf("La radice quadrata della divisione è: \n", rapporto);
+    } while(rapporto > 0);
+printf("valore non identificato perchè i numeri sono negativi");
     return 0;
 }
