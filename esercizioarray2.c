c531ad32f5cb8c7cfccb2129dd8b30fd4d621088
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2022-03-13 21:41:21 +0100

    esercizioarray2

---
+++ esercizioarray2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define DIM 5
+int main(int argc, char *argv[])
+{
+
+    int numeri[DIM];
+    int i;
+
+    
+
+
+            for (i = DIM - 1 ; i >= 0; i--)
+    {
+        if (i == DIM - 1)
+        {
+            printf("inserisci numeri:\n");
+                scanf("%d", &numeri[i]);
+        }
+        else
+            scanf("%d", &numeri[i]);
+    }
+
+    for (i = 0; i < DIM; i++)
+    {
+        if (i == 0)
+        {
+            printf("numeri inseriti sono:\n");
+                printf("%d", numeri[i]);
+        }
+        else
+            printf("%d", numeri[i]);
+    }
+
+    return 0;
+}
