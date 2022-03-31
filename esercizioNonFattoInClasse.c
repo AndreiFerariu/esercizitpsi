bea9de90866a361ecb4810c76bcd6367875cb9f9
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2022-03-31 22:13:38 +0200

    migliorato

--- esercizioNonFattoInClasse.c
+++ esercizioNonFattoInClasse.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 int SearchNumber(int argc, char *argv[], int number)
 {
-
 for(int i = 1; i< argc; i++)
 {
 if(atoi(argv[i]) == number)
 return i;
-
 }
 return -1;
-
 }
-
-
 int main(int argc, char *argv[])
 {
     int number, nFound;
-
 printf("enter a number that you want search\n");
 scanf("%d", &number);
-
 nFound = SearchNumber(argc, argv, number);
-
-if(nFound < 0 )
-printf("Number not found\n");
+if(nFound == -1 )
+printf("Number %d not found\n, number");
 else 
 printf("Number %d found in position %d\n",number, nFound);
-
 }
