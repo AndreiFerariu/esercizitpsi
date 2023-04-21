16a573d152bec7c91ab0257b51d2ec08a22841b5
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2023-04-21 13:25:49 +0200

    Add files via upload

--- es11.c
+++ es11.c
@@ -13,43 +13,43 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    int p1p2[2], pid, incremento_articolo;
-    char articolo_iniziale[50], articolo_finale[50], stringa[50], buff[50], argomento_grep[];
+    int p1p2[2], pid, nread;
+    char articolo_iniziale[10], articolo_finale[10], stringa[2], buff[50], argomento_grep[40];
 
-    pid = fork();
-    if (pid == 0)
+    while (1)
     {
-        pipe(p1p2);
-        while (1)
+        printf("Inserisci articolo: \n");
+        scanf("%s", stringa);
+        if (strcmp(stringa, "esci") == 0)
         {
-            printf("Inserisci articolo: \n");
-            scanf("%s", stringa);
-            if (strcmp(stringa, "esci") == 0)
-            {
-                close(p1p2[1]);
-                printf("Esecuzione terminata, esco.\n");
-                exit(0);
-            }
-            
-            write(p1p2[1], stringa, sizeof(stringa));
-
-              pid = fork();
-            if (pid == 0)
-            {
-                 read(p1p2[0], buff, sizeof(buff));
-                 sprintf(articolo_iniziale, "ART. %s.", buff);
-                 sprintf(articolo_finale, "ART. %d.", atoi(buff)++);
-                 sprintf(argomento_grep, "%s. %s ",articolo_iniziale,articolo_finale);
-                execl("/usr/bin/grep", "-z", "-o", argomento_grep, argv[1], (char *)0);
-
-                return -1;
-            }
-        
+            printf("Esecuzione terminata, esco.\n");
+            exit(0);
         }
-        
+        sprintf(articolo_iniziale, "ART. %s.", stringa);
+        sprintf(articolo_finale, "ART. %d.", atoi(stringa) + 1);
+        sprintf(argomento_grep, "(?<=%s)(?s).*(?=%s)", articolo_iniziale, articolo_finale);
+        pipe(p1p2);
+        pid = fork();
+        if (pid == 0)
+        {
+            printf("ciao");
+            close(p1p2[0]);
+            close(1);
+            dup(p1p2[1]);
+            close(p1p2[1]);
+            execl("/usr/bin/grep", "-z", "-o", "-P", argomento_grep, argv[1], NULL);
+            return -1;
+        }
+
+        while (nread = read(p1p2[0], buff, strlen(buff)))
+        {
+            write(1, buff, sizeof(buff));
+        }
+        close(p1p2[1]);
+        close(p1p2[0]);
     }
-   close(p1p2[1]);
-  close(p1p2[0]);
+    close(p1p2[1]);
+    close(p1p2[0]);
 
     return 0;
 }
